7d9184fdd9e031720fc54d00d193d3dc0a8733ad
lolicon <lolicon@mail.az>
2017-06-07 15:19:21 +0800

    fetch length include push lsh

--- src/main/kotlin/kt/ruby/Array.kt
+++ src/main/kotlin/kt/ruby/Array.kt
@@ -4,9 +4,26 @@ package kt.ruby
  * Created by natsuki on 07/06/2017.
  */
 
-inline fun <T> List<T>.at(idx: Int) = this[idx]
+fun <T> List<T>.at(idx: Int) = this[idx]
 
-inline fun <T> Array<T>.at(idx: Int) = this[idx]
+fun <T> Array<T>.at(idx: Int) = this[idx]
+
+fun <T> List<T>.fetch(idx: Int, default: T) = if (idx >= size) default else this[idx]
+
+fun <T> Array<T>.fetch(idx: Int, default: T) = if (idx >= size) default else this[idx]
+
+infix fun <E> MutableList<E>.lsh(e: E) = add(e)
+
+fun <E> MutableList<E>.push(e: E) = add(e)
+
+fun <E> List<E>.include(e: E): Boolean = contains(e)
+
+inline val <T> Array<T>.length: Int
+    get() = size
+
+inline val <T> Collection<T>.length: Int
+    get() = size
+
 
 
 
--- src/test/kotlin/kt/ruby/ArraySpek.kt
+++ src/test/kotlin/kt/ruby/ArraySpek.kt
@@ -1,5 +1,6 @@
 package kt.ruby
 
+import junit.framework.Assert.assertEquals
 import junit.framework.Assert.assertTrue
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.*
@@ -12,14 +13,42 @@ import org.junit.runner.RunWith
 
 @RunWith(JUnitPlatform::class)
 class ArraySpek : Spek({
-    given("element access") {
+    given("immutable list / array") {
         val lists = listOf(1, 2, 3, 4, 5, 6, 7, 8)
         val array = arrayOf(1, 2, 3, 4, 5, 6, 7, 8)
 
-        on("at") {
+        on(" methods / property ") {
             it("should be the 3rd element of list/array") {
                 assertTrue(lists.at(3) == 4)
                 assertTrue(array.at(3) == 4)
+            }
+
+            it("should be the default value when fetch out of range") {
+                assertTrue(lists.fetch(3, 100) == 4)
+                assertTrue(array.fetch(3, 100) == 4)
+                assertEquals(lists.fetch(100, 2000), 2000)
+            }
+
+            it("should report size") {
+                assertEquals(lists.length, lists.size)
+                assertEquals(array.length, array.size)
+
+                assertEquals(lists.include(4), true)
+                assertEquals(lists.include(10), false)
+            }
+        }
+    }
+
+
+    given("mutable list") {
+        on("methods / property ") {
+            it("should add 100 ") {
+                val lists = mutableListOf(1, 2, 3, 4, 5, 6, 7)
+                lists.push(100)
+                assertTrue(lists.last() == 100)
+
+                lists lsh 101
+                assertTrue(lists.last() == 101)
             }
         }
     }

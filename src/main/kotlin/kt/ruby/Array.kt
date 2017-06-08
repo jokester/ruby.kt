fa7c95f09781d4fa37135194d6f39d0912e014be
lolicon <lolicon@mail.az>
2017-06-08 13:27:41 +0800

    arrays utilities and alias

--- src/main/kotlin/kt/ruby/Array.kt
+++ src/main/kotlin/kt/ruby/Array.kt
@@ -16,13 +16,30 @@ infix fun <E> MutableList<E>.lsh(e: E) = add(e)
 
 fun <E> MutableList<E>.push(e: E) = add(e)
 
+fun <E> MutableList<E>.unshift(e: E) = add(0, e)
+
 fun <E> List<E>.include(e: E): Boolean = contains(e)
 
-inline val <T> Array<T>.length: Int
+
+val <T> Array<T>.length
     get() = size
 
-inline val <T> Collection<T>.length: Int
+val <T> Collection<T>.length
     get() = size
+
+//val <T> Array<T>.length by alias(Array<T>::size)
+//val <T> Collection<T>.length by alias(Collection<T>::size)
+//
+//fun <R, T> alias(alias: KProperty1<R, T>) = object : ReadOnlyProperty<R, T> {
+//    override operator fun getValue(thisRef: R, property: KProperty<*>): T {
+//        return alias.get(thisRef)
+//    }
+//}
+
+
+
+
+
 
 
 
--- src/test/kotlin/kt/ruby/ArraySpek.kt
+++ src/test/kotlin/kt/ruby/ArraySpek.kt
@@ -36,6 +36,8 @@ class ArraySpek : Spek({
                 assertEquals(lists.include(4), true)
                 assertEquals(lists.include(10), false)
             }
+
+
         }
     }
 
@@ -49,8 +51,61 @@ class ArraySpek : Spek({
 
                 lists lsh 101
                 assertTrue(lists.last() == 101)
+
+                lists.unshift(0)
+                assertTrue(lists.first() == 0)
+
+                val pop = lists.pop()
+                assertEquals(pop, 101)
+                assertEquals(lists.size, 9)
+
+                val shift = lists.shift()
+                assertEquals(shift, 0)
+                assertEquals(lists.size, 8)
+
+                val deleteAt = lists.deleteAt(2)
+                assertEquals(deleteAt, 3)
+                assertEquals(lists.size, 7)
+
+                lists.add(4)
+                lists.unshift(4)
+                lists.lsh(4)
+                lists.delete(4)
+                assertEquals(lists.length, 6)
+            }
+
+            it("should trim all null for mutable") {
+                val mutableListOf = mutableListOf(1, 2, 3, 4, 5, null, 6, null)
+                mutableListOf.compact()
+                val contains = mutableListOf.contains(null)
+                assertTrue(!contains)
+            }
+
+            it("should return arrays/lists without null"){
+                val list = listOf(1,2,3,4,5,6,null,7,8,null,9)
+                val array = arrayOf(1,2,3,4,5,6,null,7,8,null,9)
+
+                val compactList = list.compact()
+                val compactArray = array.compact()
+
+                assertTrue(!compactList.contains(null))
+                assertTrue(!compactArray.contains(null))
             }
         }
     }
 })
+
+fun <E> MutableList<E>.delete(element: E): E? = if (remove(element)) element else null
+
+fun <E> MutableList<E>.deleteAt(position: Int): E = removeAt(position)
+
+fun <T> MutableList<T>.shift() = removeAt(0)
+
+fun <T> MutableList<T>.pop() = removeAt(length - 1)
+
+fun <T> MutableList<T>.compact() = removeIf { it == null }
+
+fun <T> List<T>.compact() = filter { it != null }
+
+fun <T> Array<T>.compact() = filter { it != null }
 
